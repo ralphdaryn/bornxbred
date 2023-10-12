3e3f0687323c98375dcaf7549bcbf23e73cb1539
Ralph <ralphdarync@gmail.com>
2023-10-11 22:31:53 -0400

    update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,22 @@
+import "./Header.scss";
 import logo from "../../assets/images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   return (
     <div className="header">
-      <h1 className="header__title">bornxbred</h1>
-      <img className="header__logo" src={logo} alt="logo pic" />
+      <div className="header__container">
+        <img className="header__logo" src={logo} alt="logo pic" />
+        <h1 className="header__title">bornxbred</h1>
+        <div>
+          <FontAwesomeIcon className="header__searchbar" icon={faSearch} />
+          <FontAwesomeIcon className="header__cart" icon={faShoppingCart} />
+          <FontAwesomeIcon className="header__user" icon={faUser} />
+        </div>
+      </div>
     </div>
   );
 };
